4018142c196a0a2d65494e66bfdba07b7c4cdc80
Jonne Karhunen <jonne.karhunen@metropolia.fi>
2023-11-12 22:20:19 +0200

    Added functionality to fetch initial list of books at runtime

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,10 +1,19 @@
+import { useState, useEffect } from "react";
 import { BookList } from "./components/BookList";
-import dummyBooks from "../data";
+import bookService from "./services/books";
 
 function App() {
+  const [books, setBooks] = useState([]);
+
+  useEffect(() => {
+    bookService.getAll().then((initialBooks) => {
+      setBooks(initialBooks);
+    });
+  }, []);
+
   return (
     <>
-      <BookList books={dummyBooks} />
+      <BookList books={books} />
     </>
   );
 }
