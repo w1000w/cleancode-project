0711416edece06c4b6c12e9b5d4ec8a6eb6afd70
Jonne Karhunen <jonne.karhunen@metropolia.fi>
2023-11-12 21:02:54 +0200

    Added dummy data with functions to manipulate it

--- backend/models/data.js
+++ backend/models/data.js
@@ -3,39 +3,41 @@ const books = [
     title: "The Great Gatsby",
     author: "F. Scott Fitzgerald",
     year: 1925,
-    isbn: "978-3-16-148410-0",
+    isbn: "9783161484100",
   },
   {
     title: "To Kill a Mockingbird",
     author: "Harper Lee",
     year: 1960,
-    isbn: "978-3-16-148410-1",
+    isbn: "9783161484101",
   },
   {
     title: "1984",
     author: "George Orwell",
     year: 1949,
-    isbn: "978-3-16-148410-2",
+    isbn: "9783161484102",
   },
   {
     title: "Pride and Prejudice",
     author: "Jane Austen",
     year: 1813,
-    isbn: "978-3-16-148410-3",
+    isbn: "9783161484103",
   },
   {
     title: "The Catcher in the Rye",
     author: "J.D. Salinger",
     year: 1951,
-    isbn: "978-3-16-148410-4",
+    isbn: "9783161484104",
   },
 ];
 
 function addBook(book) {
+  book.isbn = book.isbn.replace(/-/g, ""); // remove all dashes from the ISBN
   books.push(book);
 }
 
 function removeBook(isbn) {
+  isbn = isbn.replace(/-/g, ""); // remove all dashes from the ISBN
   const index = books.findIndex((book) => book.isbn === isbn);
   if (index !== -1) {
     books.splice(index, 1);
@@ -51,5 +53,3 @@ module.exports = {
   removeBook,
   getAllBooks,
 };
-
-module.exports = books;
