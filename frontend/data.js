a702cb0cec35c6a9bd7bb6b1401f10238ae88d3e
Jonne Karhunen <jonne.karhunen@metropolia.fi>
2023-11-12 21:34:28 +0200

    Added dummy data to test frontend functionality

---
+++ frontend/data.js
@@ -0,0 +1,34 @@
+const books = [
+  {
+    title: "The Great Gatsby",
+    author: "F. Scott Fitzgerald",
+    year: 1925,
+    isbn: "9783161484100",
+  },
+  {
+    title: "To Kill a Mockingbird",
+    author: "Harper Lee",
+    year: 1960,
+    isbn: "9783161484101",
+  },
+  {
+    title: "1984",
+    author: "George Orwell",
+    year: 1949,
+    isbn: "9783161484102",
+  },
+  {
+    title: "Pride and Prejudice",
+    author: "Jane Austen",
+    year: 1813,
+    isbn: "9783161484103",
+  },
+  {
+    title: "The Catcher in the Rye",
+    author: "J.D. Salinger",
+    year: 1951,
+    isbn: "9783161484104",
+  },
+];
+
+export default books;
