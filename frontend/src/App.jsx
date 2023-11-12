5d1704049720d070b25f123c70e58c31db629688
Jonne Karhunen <jonne.karhunen@metropolia.fi>
2023-11-12 22:56:52 +0200

    Added a form to add a new book

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,9 +1,48 @@
 import { useState, useEffect } from "react";
-import { BookList } from "./components/BookList";
+import BookList from "./components/BookList";
+import BookForm from "./components/BookForm";
 import bookService from "./services/books";
 
 function App() {
   const [books, setBooks] = useState([]);
+  const [bookName, setBookName] = useState("");
+  const [bookAuthor, setBookAuthor] = useState("");
+  const [bookYear, setBookYear] = useState("");
+  const [bookIsbn, setBookIsbn] = useState("");
+
+  const handleBookName = (event) => {
+    setBookName(event.target.value);
+  };
+
+  const handleBookAuthor = (event) => {
+    setBookAuthor(event.target.value);
+  };
+
+  const handleBookYear = (event) => {
+    setBookYear(event.target.value);
+  };
+
+  const handleBookIsbn = (event) => {
+    setBookIsbn(event.target.value);
+  };
+
+  const handleDelete = (isbn) => {
+    bookService.deleteBook(isbn).then(() => {
+      setBooks(books.filter((book) => book.isbn !== isbn));
+    });
+  };
+
+  const handleAdd = () => {
+    const newBook = {
+      title: bookName,
+      author: bookAuthor,
+      year: bookYear,
+      isbn: bookIsbn,
+    };
+    bookService.addBook(newBook).then((returnedBook) => {
+      setBooks([...books, returnedBook]);
+    });
+  };
 
   useEffect(() => {
     bookService.getAll().then((initialBooks) => {
@@ -13,7 +52,20 @@ function App() {
 
   return (
     <>
-      <BookList books={books} />
+      <h1>Books</h1>
+      <h2>Add a book</h2>
+      <BookForm
+        handleAdd={handleAdd}
+        handleBookName={handleBookName}
+        handleBookAuthor={handleBookAuthor}
+        handleBookYear={handleBookYear}
+        handleBookIsbn={handleBookIsbn}
+        bookName={bookName}
+        bookAuthor={bookAuthor}
+        bookYear={bookYear}
+        bookIsbn={bookIsbn}
+      />
+      <BookList books={books} handleDelete={handleDelete} />
     </>
   );
 }
