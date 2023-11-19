682c5c25ee0bb1888dc60c2986642d5f25359b5a
Jonne Karhunen <jonne.karhunen@metropolia.fi>
2023-11-19 20:27:52 +0200

    Added a simple title based filtering of books

--- frontend/src/components/BookList.jsx
+++ frontend/src/components/BookList.jsx
@@ -1,13 +1,37 @@
 import Book from "./Book";
 
+import { useState } from "react";
+
 const BookList = ({ books, handleDelete }) => {
+  const [searchTerm, setSearchTerm] = useState("");
+
+  const handleSearch = (event) => {
+    setSearchTerm(event.target.value);
+  };
+
+  const filteredBooks = books.filter((book) =>
+    book.title.toLowerCase().includes(searchTerm.toLowerCase())
+  );
+
   return (
     <>
-      {books.map((book) => (
-        <div key={book.isbn}>
-          <Book {...book} handleDelete={handleDelete} />
-        </div>
-      ))}
+      <h2>Filter</h2>
+      <input
+        type="text"
+        placeholder="Search books..."
+        value={searchTerm}
+        onChange={handleSearch}
+      />
+      <hr />
+      {filteredBooks.length === 0 ? (
+        <p>No books found</p>
+      ) : (
+        filteredBooks.map((book) => (
+          <div key={book.isbn}>
+            <Book {...book} handleDelete={handleDelete} />
+          </div>
+        ))
+      )}
     </>
   );
 };
