e29ad3fed2e3e5a4e8c5521f5c6ec2e1e78cde60
Jonne Karhunen <jonne.karhunen@metropolia.fi>
2023-11-26 17:51:01 +0200

    Moved dummy data to it's own folder

---
+++ backend/data/dummyBooks.js
@@ -0,0 +1,37 @@
+books = [
+  {
+    id: 1,
+    title: "The Great Gatsby",
+    author: "F. Scott Fitzgerald",
+    year: 1925,
+    isbn: "9783161484100",
+  },
+  {
+    id: 2,
+    title: "To Kill a Mockingbird",
+    author: "Harper Lee",
+    year: 1960,
+    isbn: "9783161484101",
+  },
+  {
+    id: 3,
+    title: "1984",
+    author: "George Orwell",
+    year: 1949,
+    isbn: "9783161484102",
+  },
+  {
+    id: 4,
+    title: "Pride and Prejudice",
+    author: "Jane Austen",
+    year: 1813,
+    isbn: "9783161484103",
+  },
+  {
+    id: 5,
+    title: "The Catcher in the Rye",
+    author: "J.D. Salinger",
+    year: 1951,
+    isbn: "9783161484104",
+  },
+];
